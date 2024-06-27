a439b3c458992116b1a5d5f2847694b8ce658096
ibet1 <contact.ibet10raya@gmail.com>
2024-06-27 12:32:51 -0400

    S6 Update - add initial enableValidation function

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,1 +1,20 @@
-console.log("Hello, validation.js");
+const configItems = {
+  formSelector: ".modal__form",
+  inputSelector: ".modal__input",
+  submitButtonSelector: ".modal__button",
+  inactiveButtonClass: "modal__button_disabled",
+  inputErrorClass: "modal__input_type_error",
+  errorClass: "modal__error_visible",
+};
+
+function enableValidation(optionInputs) {
+  const formElements = [...document.querySelectorAll(configItems.formSelector)];
+  formElements.forEach((formElement) => {
+    formElement.addEventListener("submit", (e) => {
+      e.preventDefault();
+    });
+    //console.log(formElements);
+  });
+}
+
+enableValidation(configItems);
