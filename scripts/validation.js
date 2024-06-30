7d70b3d1b56b49a068e64b1b5356a6cca967f8aa
ibet1 <contact.ibet10raya@gmail.com>
2024-06-30 15:05:24 -0400

    S6 Update - revision 1: fix validation.js

--- scripts/validation.js
+++ scripts/validation.js
@@ -57,9 +57,9 @@ function toggleButtonState(
 }
 
 function setEventListerners(formElement, options) {
-  const { inputSelector } = options;
+  const { inputSelector, submitButtonSelector } = options;
   const inputElements = [...formElement.querySelectorAll(inputSelector)];
-  const submitButton = formElement.querySelector(".modal__button");
+  const submitButton = formElement.querySelector(submitButtonSelector);
 
   inputElements.forEach((inputElement) => {
     inputElement.addEventListener("input", (e) => {
