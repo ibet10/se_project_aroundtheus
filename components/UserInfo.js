c223563665095af91cbfc2d03c1bb87ed782b7f4
ibet1 <contact.ibet10raya@gmail.com>
2024-08-20 15:55:17 -0400

    S8 Update - add UserInfo Class

--- components/UserInfo.js
+++ components/UserInfo.js
@@ -1,23 +1,5 @@
-//UserInfo Class
 //export to index.js
 
-/*
-The UserInfo class is responsible for rendering information about the user on the page. 
-
-This class should:
-
-1. Take an object with the selectors of two elements into the constructor: 
- a. one for the profile’s name element and 
- b. one for its job element.
-
-2. Have a public method named getUserInfo(), which returns an object containing information about the user. 
- a. this method will be handy for cases when it's necessary to display the user data in the open form.
-
-3. Have a public method named setUserInfo(), which takes new user data and adds it to the page. 
- a. this method should be used after successful submission of the profile form.
-
-(In index.js) Create an instance of the UserInfo class in index.js and use its methods as described.
-*/
 export default class UserInfo {
   constructor({ nameSelector, jobSelector }) {
     this._nameElement = document.querySelector(nameSelector);
@@ -29,11 +11,15 @@ export default class UserInfo {
     //returns object containing user info
     return {
       //object with name and job
+      name: this._nameElement.textContent,
+      job: this._jobElement.textContent,
     };
   }
 
   //Public method setUserInfo()
   setUserInfo({ name, job }) {
     //use after successful submission of the profile form
+    this._nameElement.textContent = name;
+    this._jobElement.textContent = job;
   }
 }
